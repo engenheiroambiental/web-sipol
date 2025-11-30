4b611c6de614b8d45e2f29530beb3f22a9f25554
Jonatas Marques <33204474+engenheiroambiental@users.noreply.github.com>
2025-11-30 05:12:46 +0000

    add `/api/v1/status` endpoint

---
+++ tests/integration/api/v1/status/get.test.js
@@ -0,0 +1,5 @@
+// Não entendi muito bem a razão desse teste
+test("GET to /api/v1/status should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
+});
